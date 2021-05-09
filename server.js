30004280e8a0facfa53166b47525d5e77f8089c6
shenshuai <shenshuai89@qq.com>
2021-05-09 11:36:58 +0800

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const HTTP = require('http');
+const users = [
+  {id: 1, name: 'CI持续集成'},
+  {id: 2, name: 'CD持续部署'}
+];
+const server = HTTP.createServer((req,res) => {
+  res.setHeader('Access-Control-Allow-Origin', "*")
+  if(req.url === "/api/users"){
+    res.end(JSON.stringify(users));
+  }else{
+    res.end("Not Found")
+  }
+})
+server.listen(3000, ()=>{
+  console.log("server started...");
+})
